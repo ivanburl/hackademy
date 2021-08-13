30dcf8008db8dbce2f87497e54f31fb90e0b429e
Ivan Burlutskiy <ivandr2004@gmail.com>
2021-08-14 00:50:29 +0300

    ex08 done and tested

--- courses/cunix/ex08/src/binary_tree.c
+++ courses/cunix/ex08/src/binary_tree.c
@@ -1,6 +1,7 @@
 #include"binary_tree.h"
 #include<stdlib.h>
 #include<stdio.h>
+#include<string.h>
 
 node_t  *allocnode() {
     node_t *tmp = (node_t *) malloc(sizeof(node_t));
@@ -24,7 +25,8 @@ node_t  *insert(node_t *root, char *key, void *data)
 
     while(cur!=NULL) {
         prev=cur;
-        if (*(cur->key)>=*key)
+        int t=strcmp(prev->key,key);
+        if (t>=0)
         {
             cur=cur->left;
         }
@@ -37,25 +39,26 @@ node_t  *insert(node_t *root, char *key, void *data)
     cur = allocnode();
     cur->key=key;
     cur->data=data;
-    if (prev!=NULL)
+    
+    
+    int t=strcmp(prev->key,key);
+    if (t>=0) 
     {
-        if (*(prev->key)>=*key) 
-        {
-            prev->left=cur; 
-        }
-        else 
-        {
-            prev->right=cur;
-        }
+        prev->left=cur; 
     }
+    else 
+    {
+        prev->right=cur;
+    }
+    
 
-    return cur;
+    return root;
 }
 
 void    print_node(node_t *node) {
     if (node==NULL) return;
-    if (node->left!=NULL) print_node(node->left);
-    if (node->right!=NULL) print_node(node->right);
+    //if (node->left!=NULL) print_node(node->left);
+    //if (node->right!=NULL) print_node(node->right);
     printf("%s %s\n",node->key,(char *) node -> data);
 }
 
@@ -69,4 +72,5 @@ void    destroy_tree(node_t *node, void (*fdestroy)(node_t *root)) {
     if (node->left!=NULL) destroy_tree(node->left,fdestroy);
     if (node->right!=NULL) destroy_tree(node->right,fdestroy);
     fdestroy(node);
+    free(node);
 }
